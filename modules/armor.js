1bd095646e266639c4de0bf45447aba4107fdd61
Progamit <140381653+Progamit@users.noreply.github.com>
2023-10-03 01:49:30 +0300

    Update armor.js

--- modules/armor.js
+++ modules/armor.js
@@ -1,46 +1,46 @@
 module.exports = [
     {
         category: "ARMOR",
-        name: "A",
+        name: "A1",
         image: "https://media.istockphoto.com/id/1391325116/photo/samurai-sits-on-one-knee-in-the-smoke-3d-illustration.webp?b=1&s=170667a&w=0&k=20&c=8ktiQXWZkFI-TtaIwTJbzi-HXii__tjE9PDZOuPgKIw="
     },
     {
         category: "ARMOR",
-        name: "AA",
+        name: "AA2",
         image: "https://media.istockphoto.com/id/1251355287/photo/medieval-knight-armor-isolated.webp?b=1&s=170667a&w=0&k=20&c=A3lelI7TX0FK-jZnIphI8_wgNZbddmjPIK2vL1fjmvo="
     },
     {
         category: "ARMOR",
-        name: "AAA",
+        name: "AAA3",
         image: "https://media.istockphoto.com/id/1392984356/photo/soldier-in-gloves-holding-miltary-armor-vest.webp?b=1&s=170667a&w=0&k=20&c=CaFqaaLt00Z6lP2abscPy-1M3TUvO_egqNGNEj2_qhE="
     {
         category: "ARMOR",
-        name: "B",
+        name: "B1",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiWtaKNmxfI6FfMcmctbc0AeW9-luFbh-NOA&usqp=CAU"
     },
     {
         category: "ARMOR",
-        name: "BB",
+        name: "BB2",
         image: "https://media.istockphoto.com/id/1406627702/photo/protection.webp?b=1&s=170667a&w=0&k=20&c=NNt9CQgqhtyJQnTH9wa8J0KBTFh2nifeAE54JxDCjxs="
     },
     {
         category: "ARMOR",
-        name: "C",
+        name: "C1",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCUXoB9IESBPh-Dj3hCeafXJEOCscxotXIOg&usqp=CAU"
     },
     {
         category: "ARMOR",
-        name: "CC",
+        name: "CC2",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVgCb23ZEWDeZEf0zaxukeUdoxr9ok-2-IFA&usqp=CAU"
     },
     {
         category: "ARMOR",
-        name: "D",
+        name: "D1",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8O-s_dT6liYdSBDzkRWYMvgQrpZtTxaq6cQ&usqp=CAU"
     },
     {
         category: "ARMOR",
-        name: "W",
+        name: "WW2",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRORc5Nkcvq3EAT5lrs9VV16ugpx-1nj0ZzKQ&usqp=CAU"
     },
 ]
